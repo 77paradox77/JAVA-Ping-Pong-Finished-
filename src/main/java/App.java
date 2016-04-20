f4f876f33deb9bc4034702b24e5d17e7249e0dec
Mike <mike@epicodus.com>
2016-04-19 17:40:27 -0700

    add results route to show list returned by runPingPong method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
@@ -13,6 +14,21 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/results.vtl");
+
+      String inputtedNumberAsString = request.queryParams("userNumber");
+      Integer inputtedNumber = Integer.parseInt(inputtedNumberAsString);
+
+      PingPong myPingPong = new PingPong();
+      ArrayList<Object> results = myPingPong.runPingPong(inputtedNumber);
+
+      model.put("results", results);
+
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
