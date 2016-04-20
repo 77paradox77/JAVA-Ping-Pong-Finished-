94140357eac58e7f4b18ab51246bb832e33a1fd0
Mike <mike@epicodus.com>
2016-04-19 17:33:22 -0700

    return list of numbers up to a given number

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -3,7 +3,11 @@ import java.util.ArrayList;
 public class PingPong {
 
   public ArrayList<Object> runPingPong(Integer countUpTo) {
-    return new ArrayList<Object>();
+    ArrayList<Object> myList = new ArrayList<Object>();
+    for (Integer i = 1; i <= countUpTo; i++) {
+      myList.add(i);
+    }
+    return myList;
   }
 
 }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PingPongTest {
+
+  @Test
+  public void runPingPong_countUpToGivenNumber_ArrayList() {
+    PingPong testPingPong = new PingPong();
+    ArrayList<Object> expected = new ArrayList<Object>();
+    expected.add(1);
+    expected.add(2);
+    assertEquals(expected, testPingPong.runPingPong(2));
+  }
 
 }
